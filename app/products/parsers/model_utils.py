379ecc63b788067251f06ba8a68543da1d2a8cc0
honggyeonghyeon <hyeon21owo@gmail.com>
2026-02-25 10:05:36 +0900

    fix: selenium 변경

--- app/products/parsers/model_utils.py
+++ app/products/parsers/model_utils.py
@@ -4,13 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from transformers import AutoTokenizer, AutoModel
 
 def mean_pool(last_hidden_state, attention_mask):
